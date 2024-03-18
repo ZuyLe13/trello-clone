bc9b6a6caa3c9ae5166c69dcf76f28099dedef38
Dile <duyle131103@gmail.com>
2024-03-18 11:06:50 +0700

    Dark Light Mode

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,23 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        // primary: {
+        //   main: '#ff5252'
+        // }
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-  },
-});
+    dark: {
+      palette: {
+        // primary: {
+        //   main: '#000'
+        // }
+      }
+    }
+  }
+  // ...other properties
+})
 
 export default theme

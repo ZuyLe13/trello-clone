f8ca1dc674843c2386b49f7815de5773c6212619
Dile <duyle131103@gmail.com>
2024-05-09 12:15:10 +0700

    Add drag drop columns

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,22 +1,64 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import { DndContext, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // Nếu dùng PointerSensor mặc định thì phải kết hợp thuộc tính CSS touch-action: 'none' ở những phần tử kéo thả
+  // nhưng còn bug
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+
+  // Yêu cầu chuột di chuyển 10px thì mới kích hoạt event, fix trường hợp click bị gọi event
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+
+  // Nhấn giữ 250ms và dung sai của cảm ứng 5px thì mới kích hoạt event
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 5 } })
+
+  // Ưu tiên sử dụng kết hợp 2 loại sensor là mouse và touch để có trải nghiệm trên mobile tốt nhất
+  // const sensors = useSensors(pointerSensor)
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+
+    // Kiểm tra nếu không tồn tại over (kéo linh tinh ra ngoài thì return tránh lỗi)
+    if (!over) return
+
+    if (active.id !== over.id) {
+      // Lấy vị trí cũ từ active, vị trí mới từ over
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+
+      // Dùng để xử lý gọi API
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
 
   return (
-    <Box sx={{
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      p: '6px 0'
-    }}>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box sx={{
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        p: '6px 0'
+      }}>
 
-      {/* List Column */}
-      <ListColumns columns={orderedColumns} />
+        {/* List Column */}
+        <ListColumns columns={orderedColumns} />
 
-    </Box>
+      </Box>
+    </DndContext>
   )
 }
 

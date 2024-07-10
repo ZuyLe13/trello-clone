0923fea2941bda67325732b32f732198a64fe3e6
Dile <duyle131103@gmail.com>
2024-07-10 18:01:10 +0700

    Update constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ if (process.env.BUILD_MODE === 'dev') {
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-21ys.onrender.com/'
+  apiRoot = 'https://trello-api-21ys.onrender.com'
 }
 
 export const API_ROOT = apiRoot
